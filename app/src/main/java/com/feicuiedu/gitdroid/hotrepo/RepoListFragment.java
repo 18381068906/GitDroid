59c445bbc954a76c4d3ad01984580fffb73e880b
unknown <>
2016-07-27 21:54:19 +0800

    添加了热点仓库工具类处理数据加载（假数据）

--- app/src/main/java/com/feicuiedu/gitdroid/hotrepo/RepoListFragment.java
+++ app/src/main/java/com/feicuiedu/gitdroid/hotrepo/RepoListFragment.java
@@ -8,18 +8,30 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.feicuiedu.gitdroid.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import in.srain.cube.views.ptr.PtrClassicFrameLayout;
+import in.srain.cube.views.ptr.PtrDefaultHandler;
+import in.srain.cube.views.ptr.PtrFrameLayout;
+import in.srain.cube.views.ptr.header.StoreHouseHeader;
 
 /**
  * Created by DELL on 2016/7/27.
  */
 public class RepoListFragment extends Fragment {
     @BindView(R.id.lvRepos)ListView listView;
+    @BindView(R.id.emptyView) TextView emptyView;
+    @BindView(R.id.errorView) TextView errorView;
+    @BindView(R.id.ptrClassicFrameLayout) PtrClassicFrameLayout ptrFrameLayout;
     private ArrayAdapter<String> adapter;
+    private RepoListPresenter presenter;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -30,13 +42,65 @@ public class RepoListFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.bind(this, view);
-        String[] datas = {
-                "aaaaaaaaaa",
-                "bbbbbbbbbbbbbb",
-                "ccccccccccccc",
-                "dddddddddddd"};
+        presenter = new RepoListPresenter(this);
+        ArrayList<String> datas = new ArrayList<>();
+        datas.add("1111");
+        datas.add("2222");
+        datas.add("3333");
+        datas.add("4444");
         adapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,datas);
         listView.setAdapter(adapter);
-
+        init();
     }
+
+    private void init() {
+        //设置刷新间隔冲突
+        ptrFrameLayout.setLastUpdateTimeRelateObject(this);
+        // 关闭header所用时长
+        ptrFrameLayout.setDurationToCloseHeader(1500);
+        // 下拉刷新监听处理
+        ptrFrameLayout.setPtrHandler(new PtrDefaultHandler() {
+            @Override public void onRefreshBegin(PtrFrameLayout frame) {
+                // 在工具类中加载数据，加载完成后回调本类中的方法刷新视图
+                presenter.refresh();
+            }
+        });
+        // 不影响程序。只完善视图
+        StoreHouseHeader header = new StoreHouseHeader(getContext());
+        header.initWithString("I LIKE " + " JAVA");
+        header.setPadding(0, 60, 0, 60);
+
+        // 修改Ptr的HeaderView效果
+        ptrFrameLayout.setHeaderView(header);
+        ptrFrameLayout.addPtrUIHandler(header);
+        ptrFrameLayout.setBackgroundResource(R.color.colorRefresh);
+    }
+    public void showContentView() {
+        ptrFrameLayout.setVisibility(View.VISIBLE);
+        emptyView.setVisibility(View.GONE);
+        errorView.setVisibility(View.GONE);
+    }
+
+    public void showErrorView(String errorMsg) {
+        ptrFrameLayout.setVisibility(View.GONE);
+        emptyView.setVisibility(View.GONE);
+        errorView.setVisibility(View.VISIBLE);
+    }
+
+    public void showEmptyView() {
+        ptrFrameLayout.setVisibility(View.GONE);
+        emptyView.setVisibility(View.VISIBLE);
+        errorView.setVisibility(View.GONE);
+    }
+
+    public void stopRefresh() {
+        ptrFrameLayout.refreshComplete();
+    }
+
+    public void refreshData(List<String> data) {
+        adapter.clear();
+        adapter.addAll(data);
+        adapter.notifyDataSetChanged();
+    }
+
 }
